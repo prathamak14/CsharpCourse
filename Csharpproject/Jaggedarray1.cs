ec1df79f9a12b837b9f99f5faa2db36705db79c1
prathamak14 <prathamak60@gmail.com>
2026-09-22 10:25:45 -0700

    Merge remote changes

---
+++ Csharpproject/Jaggedarray1.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Jaggedaeeay
+{
+    public static void ArrayJagged()
+    {
+            int [][] = new int[3][];
+
+            arr[0] = new int[] {10 , 11 ,12 ,13};
+            arr[1]=new int[] {15 , 20 , 25 , 30};
+
+            for ( int ~ ) 
+
+
+    }
+}
